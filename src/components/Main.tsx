fd0b574ab6a93748c4dcf0e8b56b1986c2e26fbc
hussainhafidh <hussainhafidh1@gmail.com>
2024-07-01 11:58:58 +0530

    Fix linting errors and update caniuse-lite

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -109,9 +109,13 @@ const Main: React.FC = () => {
                 <button 
                     className="bg-slate-100 mr-3 py-2 px-4 text-sm rounded hover:border"
                     onClick={(e) => onSearchSubmit(e)}>Google Search</button>
-                <button 
+                {/* <button 
                     className="bg-slate-100 py-2 px-4 text-sm  rounded hover:border"
-                    onClick={() => router.push('https://www.google.com/doodles')}>I'm Feeling Lucky</button>
+                    onClick={() => router.push('https://www.google.com/doodles')}>I'm Feeling Lucky</button> */}
+
+             <button 
+                className="bg-slate-100 py-2 px-4 text-sm  rounded hover:border"
+                onClick={() => router.push('https://www.google.com/doodles')}>I&apos;m Feeling Lucky</button>
             </div>
         </div>
     )
